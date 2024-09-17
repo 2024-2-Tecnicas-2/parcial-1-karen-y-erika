de2749b964a64f73374047e7a799b771ce00e3c6
Paula <paulaandrea28112001@gmail.com>
2024-09-16 21:29:14 -0500

    cambios :(

--- src/main/java/com/mycompany/parcial1/Principal.java
+++ src/main/java/com/mycompany/parcial1/Principal.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Principal {
 
+    static ArrayList Productos = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         int opc;
@@ -34,30 +37,36 @@ public class Principal {
 
     }
 
-     public static void verProductos(int opc) {
-        Revista revista = new Revista();
-        Libro libro = new Libro();
-        System.out.println("");
+    public static void verProductos(int opc) {
 
-        switch (opc) {
-            case 1:
-                System.out.println("Ingrese el nombre de la revista: ");
-                String Nombre = sc.nextLine();
-                revista.setTitulo(Nombre);
-                System.out.println("Ingrese el año de publicación: ");
-                int ano = Integer.parseInt(sc.nextLine());
-                revista.setAnoPublicacion(ano);
-                System.out.println("Ingrese el numero de la revista: ");
-                int numbrevista = Integer.parseInt(sc.nextLine());
-                revista.setNumeroRevistas(numbrevista);
-                System.out.println("Ingrese el titulo de la revista: ");
-                String nombRevista = sc.nextLine();
-                revista.setNombreRevista(nombRevista);
-                publicaciones.add(revista);
-}
+        for (int i = 0; i < productos.size(); i++) {
+            System.out.println(i + " : " + productos.get(i).toString());
+        }
 
+    }
 
+    public static void RealizarPedido(int opc) {
+        Laptop laptop = new Laptop();
+        Telefono telefono = new Telefono();
+        Audifonos audifonos = new Audifonos;
+        System.out.println("Seleccione el producto que desea comprar");
 
-    
-    
-}
+        while (true) {
+            System.out.println("1.Telefono");
+            System.out.println("2.Laptop");
+            System.out.println("3.Audifonos");
+            opc = Integer.parseInt(sc.nextLine());
+
+            if (opc == 1) {
+                Telfono(opc);
+            } else if (opc == 2) {
+                Laptop(opc);
+
+            } else if (opc == 3) {
+                Audifonos(opc);
+
+            } else {
+                System.out.println("Por favor ingrese una opcion correcta");
+            }
+
+           
